1792f5396a3b0037edfd23c7ac88e4603bffa8df
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-07-08 02:22:03 +0530

    update gitignore

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API = "AIzaSyBK5dOhiZWmYEa1TPZ6aAvb-Fv5VK367qY";
+const GOOGLE_API = process.env.REACT_APP_GOOGLE_API;
 
 export const VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
@@ -8,4 +8,6 @@ export const YOUTUBE_SEARCH_API_LINK =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const YOUTUBE_SEARCH_QUERY =
-  " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=AIzaSyBK5dOhiZWmYEa1TPZ6aAvb-Fv5VK367qY&q=";
+  " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&key=" +
+  GOOGLE_API +
+  "&q=";
